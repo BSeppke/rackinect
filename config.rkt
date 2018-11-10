3524a9576ebcf5cb2f9441e656c49783fca94518
Benjamin Seppke <benjamin.seppke@gmail.com>
2018-11-10 22:14:34 +0100

    changed installation procedure. Now, it works like in vigracket. also restructured c-binding, added doc generation and fixed char pointer issue

--- config.rkt
+++ config.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require ffi/unsafe)
+
 ;; Module constants
 (define rackinect-path (collection-path "rackinect"))
 (define rackinect-version "1.0.0")
@@ -10,12 +12,51 @@
           ((equal? (system-type 'os) 'macosx)  "libkinect-grab_c.dylib")
           ((equal? (system-type 'os) 'unix)    "libkinect-grab_c.so")
           (else (error "Only macosx, windows and unix are supported"))))
+
 (define rackinect-dylib-path (build-path rackinect-path rackinect-dylib-file))
 
- 
+;; The compilation routine (at least for macosx and unix)
+(define (rebuild-rackinect)
+  (define old_path (current-directory))
+  (define rackinect-grab_c-path (build-path rackinect-path "kinect-grab_c"))
+  (define racket-bits (* 8 (ctype-sizeof _pointer)))
+  (begin
+    (if (or (equal? (system-type 'os) 'macosx)
+            (equal? (system-type 'os) 'unix))
+        (begin 
+          (display "-------------- BUILDING LIBFREENECT-C-WRAPPER FOR KINECT GRABBING --------------")
+          (newline)
+          (current-directory rackinect-grab_c-path)
+          (if (system* (format "build-~a.sh" racket-bits))
+              (begin
+                (copy-file (build-path (current-directory) "bin"  rackinect-dylib-file) rackinect-dylib-path #t)
+                #t)
+              (error "making the rackinect-grab_c lib failed, although vigra seems to be installed"))
+          (current-directory old_path))
+        ;;For windows: Just copy the correct binaries (no system called needed herein
+        (let ((bindir     (build-path rackinect-grab_c-path "bin" (string-append "win"(number->string racket-bits)))))
+          (if (equal? (system-type 'os) 'windows)
+              (let* ((binaries   (find-files (compose (curry equal?  #".dll") path-get-extension)  bindir))
+                     (result     (map (lambda (f) (copy-file f (path->string (build-path rackinect-path (file-name-from-path f))) #t)) binaries)))
+                (if (foldl (lambda (x y) (and x y)) #t result)
+                    #t
+                    (error (string-append "Copying of rackinect-grab_c's binaries from " (path->string bindir) " to "  (path->string rackinect-path) " failed !"))))
+              (error "Only windows, Mac OS X and Unix are supported!"))))
+    ;;If it still fails loading -> give up and inform the user
+    (void (ffi-lib rackinect-dylib-path #:fail (lambda() (error "The rackinect-grab_c (still) cannot be loaded after one rebuild phase"))))))
+
+
 ;; For Windows: Add the dll directory to the systems path:
-(when (equal? (system-type 'os) 'windows)
-    (putenv "PATH" (string-append (path->string rackinect-path) ";" (getenv "PATH"))))
+(void (when (equal? (system-type 'os) 'windows)
+        (putenv "PATH" (string-append (path->string rackinect-path) ";" (getenv "PATH")))))
+
+;; For Mac OS X: Add the MacPorts directory to the systems path
+(void (when (equal? (system-type 'os) 'macosx)
+        (putenv "PATH" (string-append "/opt/local/bin" ":" (getenv "PATH")))))
+
+;;Try to load the dylib: If this fails - try to rebuild it!
+(void (ffi-lib rackinect-dylib-path #:fail rebuild-rackinect))
+
 
 (provide rackinect-path
          rackinect-version
--- install.rkt
+++ install.rkt
@@ -6,63 +6,46 @@
 (define local-install-path  
   (build-path local-collects-path "rackinect"))
 
-;; 1. Create local collects directory if not already existing
-(when (not (directory-exists? local-collects-path))
-  (make-directory* local-collects-path))
 
-;; 2. If rackinect-collects dir extist, delete it.
-(when (directory-exists? local-install-path)
-  (delete-directory/files local-install-path))
+(define (show-notes)
+  (display (format "rackinect has been successfully installed in ~a." local-install-path))(newline)
+  (display "Use it by typing (require rackinect) or run the examples (provided in examples.rkt)")(newline)
+  (cond ((equal? (system-type 'os) 'windows)
+         (begin
+           (display "=== Important notes for Windows ===")(newline)
+           (display "Please make sure to install the MS Visual C++ 2012 runtimes first!")(newline)
+           (display "You may get them from: https://www.microsoft.com/en-us/download/details.aspx?id=30679")(newline)))         
+        ((equal? (system-type 'os) 'macosx)
+         (begin
+           (display "=== Important notes for Max OS X ===")(newline)
+           (display "Please install the MacPorts port system first! You get it from: https://www.macports.org")(newline)
+           (display "After the installation of MacPorts, install CMake and libusb by typing:")(newline)
+           (display "    sudo port install cmake")(newline)
+           (display "    sudo port install libusb")(newline)
+           (display "Then follow the instructions in the Readme file to install the freenect library as well.")(newline)
+           (display "The wrapper library kinect-grab_c will be build on the first load of the rackinect module!")(newline)))   
+        ((equal? (system-type 'os) 'unix)
+         (begin
+           (display "=== Important notes for Linux ===")(newline)
+           (display "Please make sure to install libusb and libfreenct first")(newline)
+           (display "You find instructions on the official libfreenect page.")(newline)
+           (display "The wrapper library kinect-grab_c will be build on the first load of the rackinect module!")(newline)))))
 
-;; 3. copy the installation contents to the local/collects/rackinect directory
-(copy-directory/files (current-directory) local-install-path)
+(define (install-rackinect)
+  (begin
+    ;; 1. Create local collects directory if not already existing
+    (when (not (directory-exists? local-collects-path))
+      (make-directory* local-collects-path))
+    ;; 2. If rackinect-collects dir extist, delete it.
+    (when (directory-exists? local-install-path)
+      (delete-directory/files local-install-path))
+    ;; 3. copy the installation contents to the local/collects/rackinect directory
+    (copy-directory/files (current-directory) local-install-path)
+    ;; 4. Tell the user about the status of the installation:
+    (show-notes)))
 
-;; 4. Load dll under windows, dylib under MacOS
-(define dylib-file
-    (cond ((equal? (system-type 'os) 'windows) "kinect-grab_c.dll")
-          ((equal? (system-type 'os) 'macosx)  "libkinect-grab_c.dylib")
-          ((equal? (system-type 'os) 'unix)    "libkinect-grab_c.so")
-          (else (error "Only macosx, windows and unix are supported"))))
-(define dylib-path (build-path local-install-path dylib-file))
-
-;; 5. For Windows: Add the dll directory to the systems path:
-(when (equal? (system-type 'os) 'windows)
-    (putenv "PATH" (string-append (path->string local-install-path) ";" (getenv "PATH"))))
-
-;; 6. Stuff needed to compile the c-bindings if necessary...
-(define base_login_script "~/.profile")
-(define kinect-grab_c-path (build-path local-install-path "kinect-grab_c"))
-
-(define login_script (if (file-exists? base_login_script)
-                         base_login_script
-                         (path->string (build-path kinect-grab_c-path "fallback.profile"))))
-
-(define login_cmd (string-append ". " login_script))
-(define (system-env arg) (system (string-append login_cmd " && " arg)))
-
-;; 7. For windows, we need to find out, which architecture DrRacket is built
-(require (only-in ffi/unsafe ctype-sizeof _pointer))
-(define racket-bits (* 8 (ctype-sizeof _pointer)))
-(define cmake_flags (if (= racket-bits 32)
-                        "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32"
-                        "-DCMAKE_BUILD_TYPE=Release"))
-
-; 8. The compilation routine (at least for macosx and unix)
- (if (or (equal? (system-type 'os) 'macosx)
-          (equal? (system-type 'os) 'unix))
-      (begin 
-        (display "-------------- BUILDING LIBFREENECT-C-WRAPPER FOR KINECT GRABBING --------------")
-        (newline)
-        (current-directory kinect-grab_c-path)
-        (if (system-env (string-append "mkdir build && cd build && cmake " cmake_flags " .. && make && cd .."))
-            (begin
-              (copy-file (build-path (current-directory) "bin" dylib-file) dylib-path #t)
-              #t)
-            (error "Making the kinect-grab_c lib failed")))
-      ;;For windows
-      (if (equal? (system-type 'os) 'windows)
-          (let ((bindir     (build-path kinect-grab_c-path "bin" (string-append "win"(number->string racket-bits)))))
-             (begin
-               (system (string-append "copy " (path->string bindir) "\\*.dll " (path->string local-install-path)))
-               #t))
-          (error "Only Mac OS X, Unix are supported for auto build of kinect_grab_c!")))
+;;Trigger the installation
+(if (member (system-type 'os) '(windows macosx unix))
+    (install-rackinect)
+    (error "Sorry, but rackinect only supports Windows, Mac OS X or Linux"))
+    
